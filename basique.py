61fa43e481a6135196ff380ed77629b39d4c14bc
w2ak <durand.clement.13@gmail.com>
2015-11-09 17:17:39 +0100

    dns orientation

--- basique.py
+++ basique.py
@@ -1,12 +1,20 @@
 from scapy.all import *
 
+class entier:
+    def __init__(self):
+        self.val = 0
+    def inc(self):
+        self.val += 1
+i = entier()
 def maFonction(a):
-    msg = str(i) + ': ' + a[IP].src + ' -> ' + a[IP].dst
+    i.inc()
+    msg = str(i.val) + ': ' + a[IP].src + ' -> ' + a[IP].dst
     if (a[IP].src == filtre) or (a[IP].dst == filtre) or (filtre == '-a'):
-    	msg = msg + ' <------------------------'
-    print msg
+        if (a[DNS].qr==0L):
+            msg = msg + ' <-- REQUEST for ' + a[DNS][0].qname
+        else:
+            msg = msg + ' <-- RESPONSE'
+        print msg
 
 filtre = sys.argv[1]
-i = 0
-a = sniff(count = 1000,lfilter = lambda x: x.haslayer(IP),prn = maFonction)
-#i = i+1
+a = sniff(count = 1000,lfilter = lambda x: x.haslayer(DNS),prn = maFonction)
