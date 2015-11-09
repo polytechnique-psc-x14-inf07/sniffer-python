615d99d889aca9d2a68ecd6f84b5832593b96556
wilhelmbh <wilhelmhb@live.fr>
2015-11-09 16:27:28 +0100

    enfin un truc qui marche ^^

--- basique.py
+++ basique.py
@@ -1,12 +1,12 @@
 from scapy.all import *
 
+def maFonction(a):
+    msg = str(i) + ': ' + a[IP].src + ' -> ' + a[IP].dst
+    if (a[IP].src == filtre) or (a[IP].dst == filtre) or (filtre == '-a'):
+    	msg = msg + ' <------------------------'
+    print msg
 
 filtre = sys.argv[1]
 i = 0
-while 1:
-	a = sniff(count = 1,lfilter = lambda x: x.haslayer(IP))[0]
-	i = i+1
-	msg = str(i) + ': ' + a[IP].src + ' -> ' + a[IP].dst
-	if (a[IP].src == filtre) or (a[IP].dst == filtre) or (filtre == '-a'):
-		msg = msg + ' <------------------------'
-	print msg
+a = sniff(count = 1000,lfilter = lambda x: x.haslayer(IP),prn = maFonction)
+#i = i+1
